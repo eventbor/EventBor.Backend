c64457ca3db20abe12bc664b2a1fa59df4bc5413
dotnetbroo <jasurbekabdunazarov04@gmail.com>
2024-01-25 00:59:34 +0500

    Added Mapper and CustomException

---
+++ EventBor.Backend.Application/Commons/Exceptions/CustomException.cs
@@ -0,0 +1,12 @@
+﻿namespace EventBor.Backend.Application.Commons.Exceptions;
+
+public class CustomException : Exception
+{
+    public int statusCode { get; set; }
+
+    public CustomException(int code, string message) : base(message)
+    {
+        statusCode = code;
+    }
+
+}
---
+++ EventBor.Backend.Application/Mappers/MapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EventBor.Backend.Application.DTOs.Categories;
+using EventBor.Backend.Domain.Entities;
+
+namespace EventBor.Backend.Application.Mappers;
+
+public class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        // Category
+        CreateMap<Category, CategoryForResultDto>().ReverseMap();
+        CreateMap<Category, CategoryForUpdateDto>().ReverseMap();
+        CreateMap<Category, CategoryForCreationDto>().ReverseMap();
+    }
+}
