a4afbc43b224923785a7524c7ecd63d004afb609
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2024-02-05 15:53:57 +0500

    CustomMiddleware added

---
+++ EventBor.Backend.API/Helpers/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace EventBor.Backend.API.Helpers;
+
+public class Response
+{
+    public int StatusCode { get; set; } = 200;
+    public string Message { get; set; }
+    public object Data { get; set; }
+}
---
+++ EventBor.Backend.API/Middlewares/ExceptionHandlerMiddkeware.cs
@@ -0,0 +1,36 @@
+﻿using EventBor.Backend.API.Helpers;
+using EventBor.Backend.Application.Exceptions;
+
+namespace EventBor.Backend.API.Middlewares;
+
+public class ExceptionHandlerMiddkeware
+{
+    private readonly RequestDelegate _next;
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (EventBorException ex)
+        {
+            context.Response.StatusCode = ex.StatusCode;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = ex.StatusCode,
+                Message = ex.Message
+            });
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsJsonAsync(new Response
+            {
+                StatusCode = 500,
+                Message = ex.Message
+            });
+        }
+    }
+}
+
